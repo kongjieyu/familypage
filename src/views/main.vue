b90d23f0af4cef2013a1e358ba9ad41e2884c636
lmenghua <lmenghua@linewell.com>
2022-08-04 18:01:31 +0800

    修改

--- src/views/main.vue
+++ src/views/main.vue
@@ -34,11 +34,12 @@ const getData = () => {
           page.desc = data.desc
           page.title = data.title
           page.footer = data.footer
-          if(screenWidth.value < 700) {
+          if( devide.value == 'phone' || devide.value == 'pad' && !isOrientation.value) {
             data.father_img && page.list.push(data.father_img)
             data.mother_img && page.list.push(data.mother_img)
             data.daughter_img && page.list.push(data.daughter_img)
-          } else {
+          }
+          if( devide.value == 'pc' || devide.value == 'pad' && isOrientation.value){
             data.mother_img && page.list.push(data.mother_img)
             data.father_img && page.list.push(data.father_img)
             data.daughter_img && page.list.push(data.daughter_img)
@@ -46,20 +47,70 @@ const getData = () => {
       })
     }catch{}
 }
-
+const devide = ref('')
+const getOs = () => {
+  let os = function () {
+			let ua = navigator.userAgent,
+				isWindowsPhone = /(?:Windows Phone)/.test(ua),
+				isSymbian = /(?:SymbianOS)/.test(ua) || isWindowsPhone,
+				isAndroid = /(?:Android)/.test(ua),
+				isFireFox = /(?:Firefox)/.test(ua),
+				isChrome = /(?:Chrome|CriOS)/.test(ua),
+				isTablet = /(?:iPad|PlayBook)/.test(ua) || (isAndroid && !/(?:Mobile)/.test(ua)) || (isFireFox &&
+					/(?:Tablet)/.test(ua)),
+				isPhone = /(?:iPhone)/.test(ua) && !isTablet,
+				isPc = !isPhone && !isAndroid && !isSymbian;
+			return {
+				isTablet: isTablet,
+				isPhone: isPhone,
+				isAndroid: isAndroid,
+				isPc: isPc
+			};
+		}();
+		if (os.isAndroid || os.isPhone) { // 手机
+			devide.value = 'phone'
+		} else if (os.isTablet) { // 平板
+      devide.value = 'pad'
+		} else if (os.isPc) { // 电脑
+      devide.value = 'pc'
+		}
+}
+const isOrientation = ref()
 onMounted(()=>{
+  getOs()
   screenWidth.value = document.body.clientWidth
+  window.addEventListener("orientationchange", function() {
+    if(window.orientation === 90){
+        isOrientation.value = true
+      }
+      if(window.orientation === 0){
+        isOrientation.value = false
+      }
+  }, false);
   window.onresize = () => {
     return (() => {
       screenWidth.value = document.body.clientWidth
     })()
   }
 })
+
 watch(()=>screenWidth.value,(newVal,oldVal)=>{
   if(newVal){
     getData()
   }
 },{ deep: true })
+
+// watch(()=>isOrientation.value,(newVal,oldVal)=>{
+//   if(newVal){
+//     getData()
+//   }
+// },{ deep: true })
+
+// watch(()=>devide.value,(newVal,oldVal)=>{
+//   if(newVal){
+//     getData()
+//   }
+// },{ deep: true })
 </script>
 <style lang="less">
 @import url(@/style/main.less);
